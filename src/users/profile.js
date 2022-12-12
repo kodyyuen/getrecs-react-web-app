226f84047c50abb2806f4c057287b011701d0030
kodyyuen <kody.yuen@gmail.com>
2022-12-12 12:45:04 -0500

    test

--- src/users/profile.js
+++ src/users/profile.js
@@ -1,31 +1,35 @@
 import { useDispatch, useSelector } from "react-redux";
-import { logoutThunk, updateUserThunk } from "./users-thunk";
+import { logoutThunk, profileThunk, updateUserThunk } from "./users-thunk";
 import { useNavigate } from "react-router";
 import { useEffect } from "react";
+import { getAlbumName, getArtistName, getDuration, getImage, getSongID, getSongLink, getSongName, getArtistLink } from "../songs/songs-helpers"
+import { Link } from "react-router-dom";
+import { findMultipleSongsBySongIDThunk } from "../songs/songs-thunks";
 import { RenderSongsList } from "../songs/songs-list";
 
 const Profile = () => {
   const navigate = useNavigate()
   const { currentUser } = useSelector((state) => state.users)
+  const { multipleDetails } = useSelector((state) => state.songs)
   const dispatch = useDispatch()
   const handleLogoutBtn = () => {
     dispatch(logoutThunk())
     navigate('/login')
   }
   useEffect(() => {
-    dispatch(updateUserThunk(currentUser.name))
+    dispatch(updateUserThunk())
+    dispatch(findMultipleSongsBySongIDThunk(currentUser.likes))
+    dispatch(profileThunk())
   }, [])
   const handleUpdateName = (newName) => {
-    dispatch(updateUserThunk({name: newName}))
+    dispatch(updateUserThunk({ name: newName }))
   }
-
   return (
     <>
       <h1>Private Profile</h1>
       {
         currentUser &&
         <h2>Welcome new user: {currentUser.username}</h2>
-
       }
       <div className="row">
         <label for="name">Name (Optional)</label>
@@ -35,16 +39,19 @@ const Profile = () => {
           placeholder="Your name"
           value={currentUser.name} />
       </div>
+      <div className="row">
+        <ul className="list-group pe-0">
+          {
+            multipleDetails && <RenderSongsList songs={multipleDetails}/>
+          }
+        </ul>
+      </div>
+
       <button
         className="btn btn-danger"
         onClick={handleLogoutBtn}>
         Logout
       </button>
-      <h1>Likes</h1>
-      {
-        currentUser &&
-        <RenderSongsList songs={currentUser.likes}/>
-      }
     </>
   )
 }
