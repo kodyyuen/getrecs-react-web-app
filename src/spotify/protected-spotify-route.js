3554df17eb86020a9347c071636cc0fc27eb83bf
kodyyuen <kody.yuen@gmail.com>
2023-08-31 13:59:17 -0400

    added logging for debugging

--- src/spotify/protected-spotify-route.js
+++ src/spotify/protected-spotify-route.js
@@ -4,6 +4,7 @@ import { Navigate } from "react-router";
 const ProtectedSpotifyRoute = ({ children }) => {
   const { spotifyProfile } = useSelector((state) => state.spotify);
   if (spotifyProfile) {
+    console.log('spotify profile')
     return (children);
   } else {
     return (<Navigate to={'/login'} />);
